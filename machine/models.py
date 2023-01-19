3047d24a5a57742ab2343eb86d115430e98b2389
CodePan1 <CodePan1@users.noreply.github.com>
2023-01-19 15:29:47 +0700

    added VendingMachine(name,location) and Product(ven_id, name, stock, price)

---
+++ machine/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+# Create your models here.
+class VendingMachine(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    vending_machine = models.ForeignKey(VendingMachine, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255)
+    stock = models.ImageField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
